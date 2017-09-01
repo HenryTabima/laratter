4bc878411b2d1d5c5281ee05e751004bf4dc9f2d
Henry Tabima G <htabima@immail.co>
2017-09-01 09:33:15 -0500

    [add] layout and bootstrap4

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,9 +7,28 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function home() {
-        return view('welcome');
-    }
-    public function about() {
-        return view('about');
+        $messages = [
+            [
+                'id' => 1,
+                'content' => 'Este es mi primer mensaje',
+                'image' => 'http://lorempixel.com/600/338?1'
+            ],
+            [
+                'id' => 2,
+                'content' => 'Este es mi segundo mensaje',
+                'image' => 'http://lorempixel.com/600/338?2'
+            ],
+            [
+                'id' => 3,
+                'content' => 'Otro mensaje más',
+                'image' => 'http://lorempixel.com/600/338?3'
+            ],
+            [
+                'id' => 4,
+                'content' => 'El último mensaje',
+                'image' => 'http://lorempixel.com/600/338?4'
+            ]
+        ];
+        return view('welcome', ['messages' => $messages]);
     }
 }
